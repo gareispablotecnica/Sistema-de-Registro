3d2d60505db8482b60d5b5e3734dd14b6c768800
Gareis Pablo <gareispablotecnica1@gmail.com>
2026-09-10 09:14:21 -0300

    7.8

---
+++ Server/src/Interface/Tabla.ts
@@ -0,0 +1,12 @@
+
+
+export interface Productos{
+    id:number,
+    codigo:string,
+    nombre:string,
+    descripcion:string,
+    talle:string,
+    precio:number,
+    stock:number,
+    imagen:string
+}
